517d9c1f23b2ff013b7d2d4adae79224954eb4d8
amyhuang95 <62624198+amyhuang95@users.noreply.github.com>
2024-09-04 11:04:52 -0700

    add method to serialize error object and extend CustomError class

--- auth/src/errors/database-connection-error.ts
+++ auth/src/errors/database-connection-error.ts
@@ -1,9 +1,17 @@
-export class DatabaseConnnectionError extends Error {
+import { CustomError } from './custom-error';
+
+export class DatabaseConnnectionError extends CustomError {
+  statusCode = 500;
   reason = 'Error connecting to database';
 
   constructor() {
     super();
 
     Object.setPrototypeOf(this, DatabaseConnnectionError.prototype);
+  }
+
+  // Serialize and return common error object
+  serializeErrors() {
+    return [{ message: this.reason }];
   }
 }
--- auth/src/errors/request-validation-error.ts
+++ auth/src/errors/request-validation-error.ts
@@ -1,11 +1,23 @@
 import { ValidationError } from 'express-validator';
+import { CustomError } from './custom-error';
 
 // Custom error
-export class RequestValidationError extends Error {
+export class RequestValidationError extends CustomError {
+  statusCode = 400;
   constructor(public errors: ValidationError[]) {
     super();
 
     // For extending a built-in class
     Object.setPrototypeOf(this, RequestValidationError.prototype);
+  }
+
+  // Process and return common error object
+  serializeErrors() {
+    return this.errors.map((error) => {
+      if (error.type === 'field') {
+        return { message: error.msg, field: error.path };
+      }
+      return { message: error.msg };
+    });
   }
 }
