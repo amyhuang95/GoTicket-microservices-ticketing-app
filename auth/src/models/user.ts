143491718ce2c0c14ebc3b42b8fb5e1053390c0c
amyhuang95 <62624198+amyhuang95@users.noreply.github.com>
2024-09-05 11:30:54 -0700

    create auth user model in mongo

---
+++ auth/src/models/user.ts
@@ -0,0 +1,41 @@
+import mongoose from 'mongoose';
+
+// An interface that describes the properties that are required to create a new User
+// for TypeScript to check the the types of the properties
+interface UserAttrs {
+  email: string;
+  password: string;
+}
+
+// An interface that describes the properties that a User Model has
+interface UserModel extends mongoose.Model<UserDoc> {
+  build(attrs: UserAttrs): UserDoc;
+}
+
+// An interface that describes the properties that a User Document has
+interface UserDoc extends mongoose.Document {
+  email: string;
+  password: string;
+}
+
+// Defines Mongoose user model
+const userSchema = new mongoose.Schema({
+  // define data type using mongoose language (not TypeScript!)
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+// Add a function to create new user
+userSchema.statics.build = (attrs: UserAttrs) => {
+  return new User(attrs);
+};
+
+// Creates new mondel
+const User = mongoose.model<UserDoc, UserModel>('User', userSchema);
+
+export { User };
