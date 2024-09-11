429be3be5a3fad2b6d9477275472f99a152c0022
amyhuang95 <62624198+amyhuang95@users.noreply.github.com>
2024-09-11 12:37:39 -0700

    add middleware to extract JWT payload and set it on current user

---
+++ auth/src/middlewares/current-user.ts
@@ -0,0 +1,41 @@
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+
+// Define UserPayload properties
+interface UserPayload {
+  id: String;
+  email: string;
+}
+
+// Reach existing type definition and make modification
+declare global {
+  namespace Express {
+    interface Request {
+      currentUser?: UserPayload;
+    }
+  }
+}
+
+/**
+ * Middleware to extract the JWT payload and set it on 'req.currentUser'
+ */
+export const currentUser = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  // Check whether the user has a JWT (check session object first)
+  if (!req.session?.jwt) {
+    return next();
+  }
+
+  // try to get the payload and set it on current user to be used by other middleware or route handler
+  try {
+    const payload = jwt.verify(
+      req.session.jwt,
+      process.env.JWT_KEY!
+    ) as UserPayload;
+    req.currentUser = payload;
+  } catch (err) {}
+  next();
+};
