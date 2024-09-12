fdba62770bc9e73b3900af685850214046ef08ef
amyhuang95 <62624198+amyhuang95@users.noreply.github.com>
2024-09-11 22:08:58 -0700

    add first tests

---
+++ auth/src/routes/__test__/signin.test.ts
@@ -0,0 +1,57 @@
+import request from 'supertest';
+import { app } from '../../app';
+
+// Valid case - sign in with correct credential
+it('responds with a cookie when given valid credentials', async () => {
+  // create a user
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'password',
+    })
+    .expect(201);
+
+  // sign in
+  const response = await request(app)
+    .post('/api/users/signin')
+    .send({
+      email: 'test@test.com',
+      password: 'password',
+    })
+    .expect(200);
+
+  expect(response.get('Set-Cookie')).toBeDefined();
+});
+
+// Invalid case - sign in with email that does not exist
+it('fails when an email that does not exist is supplied', async () => {
+  await request(app)
+    .post('/api/users/signin')
+    .send({
+      email: 'test@test.com',
+      password: 'password',
+    })
+    .expect(400);
+});
+
+// Invalid case - sign in with wrong password
+it('fails when an incorrect password is supplied', async () => {
+  // create a user
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'password',
+    })
+    .expect(201);
+
+  // sign in with wrong password
+  await request(app)
+    .post('/api/users/signin')
+    .send({
+      email: 'test@test.com',
+      password: 'passwor',
+    })
+    .expect(400);
+});
