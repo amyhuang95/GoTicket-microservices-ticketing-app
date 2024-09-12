0edd04135060822ca1d68d7c3dc7c6de09d2b538
amyhuang95 <62624198+amyhuang95@users.noreply.github.com>
2024-09-11 20:42:16 -0700

    refactor the building of the application to app.ts

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,41 +1,5 @@
-import express from 'express';
-import 'express-async-errors';
-import { json } from 'body-parser';
 import mongoose from 'mongoose';
-import cookieSession from 'cookie-session';
-
-import { currentUserRouter } from './routes/current-user';
-import { signinRouter } from './routes/signin';
-import { signoutRouter } from './routes/signout';
-import { signupRouter } from './routes/signup';
-import { errorHandler } from './middlewares/error-handlers';
-import { NotFoundError } from './errors/not-found-error';
-
-const app = express();
-// make sure express knows that we are using customized cookie session beyond ingress nginx
-app.set('trust proxy', true);
-
-app.use(json());
-
-app.use(
-  cookieSession({
-    // disable encryption in the cookie
-    signed: false,
-    // require cookie only be used if user is visiting on https connection
-    secure: true,
-  })
-);
-
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
-
-app.all('*', async () => {
-  throw new NotFoundError();
-});
-
-app.use(errorHandler);
+import { app } from './app';
 
 const start = async () => {
   // Check environment variable is defined
