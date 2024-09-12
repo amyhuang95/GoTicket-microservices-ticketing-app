fc567d0d15203fb218764124f6e0d53be8339995
amyhuang95 <62624198+amyhuang95@users.noreply.github.com>
2024-09-11 21:38:00 -0700

    add tests for invalid cases

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -1,7 +1,7 @@
 import request from 'supertest';
 import { app } from '../../app';
 
-// Send post request with email and password to '/api/users/signup'
+// Test valid email and password
 // expect return 201 status code
 it('returns a 201 on successful signup', async () => {
   return request(app)
@@ -11,4 +11,46 @@ it('returns a 201 on successful signup', async () => {
       password: 'password',
     })
     .expect(201);
+});
+
+// Test invalid email
+// Expect return 400 status code
+it('returns a 400 with an invalid email', async () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'testtest.com',
+      password: 'password',
+    })
+    .expect(400);
+});
+
+// Test invalid password
+// Expect return 400 status code
+it('returns a 400 with an invalid password', async () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'p',
+    })
+    .expect(400);
+});
+
+// Test empty input for email and password
+// Expect return 400 status code
+it('returns a 400 with missing email and password', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+    })
+    .expect(400);
+
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      password: 'password',
+    })
+    .expect(400);
 });
