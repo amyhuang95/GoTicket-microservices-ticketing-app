29cdad60e6732ae8397c02d33d521014346ae5ee
amyhuang95 <62624198+amyhuang95@users.noreply.github.com>
2024-09-15 13:06:16 -0700

    create signin functionality

--- client/pages/index.js
+++ client/pages/index.js
@@ -2,9 +2,11 @@ import buildClient from '../api/build-client';
 
 // Generate components with provided data
 const LandingPage = ({ currentUser }) => {
-  console.log(currentUser);
-
-  return <h1>Landing Page</h1>;
+  return currentUser ? (
+    <h1>You are signed in</h1>
+  ) : (
+    <h1>You are not signed in</h1>
+  );
 };
 
 // Fetch data during server-side rendering process
