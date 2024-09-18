846eff10562552b9c72675f711be265ad80f1b39
amyhuang95 <62624198+amyhuang95@users.noreply.github.com>
2024-09-18 10:03:46 -0700

    create valid id for lookup in mongo db

--- tickets/src/routes/__test__/show.test.ts
+++ tickets/src/routes/__test__/show.test.ts
@@ -1,13 +1,14 @@
 import request from 'supertest';
 import { app } from '../../app';
-import { response } from 'express';
-
+import mongoose from 'mongoose';
 /**
  * Tests to verify the functionality of show ticket router.
  */
 
 it('returns a 404 if the ticket is not found', async () => {
-  await request(app).get('/api/tickets/random_id').send().expect(404);
+  // generate a random valid id for mongo db
+  const id = new mongoose.Types.ObjectId().toHexString();
+  await request(app).get(`/api/tickets/${id}`).send().expect(404);
 });
 
 it('returns the ticket if the ticket is found', async () => {
