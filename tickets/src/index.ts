c5c22fde9887fd015a22d66d8ed0798d66930482
amyhuang95 <62624198+amyhuang95@users.noreply.github.com>
2024-09-24 10:04:05 -0700

    add connection to nats

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
 import { app } from './app';
+import { natsWrapper } from './nats-wrapper';
 
 const start = async () => {
   // Check environment variable is defined
@@ -10,8 +11,20 @@ const start = async () => {
     throw new Error('MONGO_URI must be defined');
   }
 
-  // Connect to MongoDB instance
+  // Connect to Nats and MongoDB instance
   try {
+    await natsWrapper.connect(
+      'goticket',
+      'goticket-client',
+      'http://nats-srv:4222'
+    );
+    natsWrapper.client.on('close', () => {
+      console.log('NATS connection closed');
+      process.exit();
+    });
+    process.on('SIGINT', () => natsWrapper.client.close());
+    process.on('SIGTERM', () => natsWrapper.client.close());
+
     await mongoose.connect(process.env.MONGO_URI);
     console.log('Connected to MongoDB');
   } catch (err) {
