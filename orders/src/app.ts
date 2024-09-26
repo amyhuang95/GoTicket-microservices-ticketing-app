4166122df6fc1887365081098b67518697e0921e
amyhuang95 <62624198+amyhuang95@users.noreply.github.com>
2024-09-26 10:47:56 -0700

    Set up orders service skeleton code

---
+++ orders/src/app.ts
@@ -0,0 +1,41 @@
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+import { errorHandler, NotFoundError, currentUser } from '@goticket/common';
+
+import { createTicketRouter } from './routes/new';
+import { showTicketRouter } from './routes/show';
+import { indexTicketRouter } from './routes';
+import { updateTicketRouter } from './routes/update';
+
+const app = express();
+// make sure express knows that we are using customized cookie session beyond ingress nginx
+app.set('trust proxy', true);
+
+app.use(json());
+
+app.use(
+  cookieSession({
+    // disable encryption in the cookie
+    signed: false,
+    // require cookie only be used if user is visiting on https connection
+    // set secure to true when it's in dev/prod environment
+    secure: process.env.NODE_ENV !== 'test',
+  })
+);
+
+app.use(currentUser);
+
+app.use(createTicketRouter);
+app.use(showTicketRouter);
+app.use(indexTicketRouter);
+app.use(updateTicketRouter);
+
+app.all('*', async () => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
