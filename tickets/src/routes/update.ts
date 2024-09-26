2326a3c2db333347148b7c87ee7f1610d598a5de
amyhuang95 <62624198+amyhuang95@users.noreply.github.com>
2024-09-26 11:35:14 -0700

    Remove ticket version for update ticket event

--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -54,7 +54,7 @@ router.put(
       title: ticket.title,
       price: ticket.price,
       userId: ticket.userId,
-      version: 0, // to update
+      // version: 0, // to update
     });
 
     res.send(ticket);
