42adcc574b87ae9092c4fe9bf236baae5378d700
amyhuang95 <62624198+amyhuang95@users.noreply.github.com>
2024-09-27 10:35:09 -0700

    Add tests for creating a new order

---
+++ orders/src/routes/__test__/new.test.ts
@@ -0,0 +1,56 @@
+import mongoose from 'mongoose';
+import request from 'supertest';
+import { app } from '../../app';
+import { Ticket } from '../../models/ticket';
+import { Order, OrderStatus } from '../../models/orders';
+
+it('returns an error is the ticket does not exist', async () => {
+  const ticketId = new mongoose.Types.ObjectId();
+
+  await request(app)
+    .post('/api/orders')
+    .set('Cookie', global.signin())
+    .send({ ticketId })
+    .expect(404);
+});
+
+it('returns an error if the ticket is already reserved', async () => {
+  // Create a ticket and save it to the database
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 20,
+  });
+  await ticket.save();
+
+  // Create an order for the ticket
+  const order = Order.build({
+    ticket,
+    userId: 'testUserId',
+    status: OrderStatus.Created,
+    expiresAt: new Date(),
+  });
+  await order.save();
+
+  // Make a request to reserve the ticket
+  await request(app)
+    .post('/api/orders')
+    .set('Cookie', global.signin())
+    .send({ ticketId: ticket.id })
+    .expect(400);
+});
+
+it('reserves a ticket', async () => {
+  // Create a ticket and save it to the database
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 20,
+  });
+  await ticket.save();
+
+  // Make a request to reserve the ticket
+  await request(app)
+    .post('/api/orders')
+    .set('Cookie', global.signin())
+    .send({ ticketId: ticket.id })
+    .expect(201);
+});
