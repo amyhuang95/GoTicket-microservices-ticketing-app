8a16250e239a640857b8df745efb5217ae5432eb
amyhuang95 <62624198+amyhuang95@users.noreply.github.com>
2024-09-29 14:49:55 -0700

    Set up service to show a order

---
+++ orders/src/routes/__test__/show.test.ts
@@ -0,0 +1,53 @@
+import request from 'supertest';
+import { app } from '../../app';
+import { Ticket } from '../../models/ticket';
+
+it('fetches the order', async () => {
+  // Create a ticket
+  const ticket = Ticket.build({
+    title: 'test ticket title',
+    price: 50,
+  });
+  await ticket.save();
+
+  // Make a request to build an order with this ticket
+  const user = global.signin();
+  const { body: order } = await request(app)
+    .post('/api/orders')
+    .set('Cookie', user)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  // Make request to fetch the order
+  const { body: fetchOrder } = await request(app)
+    .get(`/api/orders/${order.id}`)
+    .set('Cookie', user)
+    .send()
+    .expect(200);
+
+  expect(fetchOrder.id).toEqual(order.id);
+});
+
+it("returns an error if one user tries to fetch the other user's order", async () => {
+  // Create a ticket
+  const ticket = Ticket.build({
+    title: 'test ticket title',
+    price: 50,
+  });
+  await ticket.save();
+
+  // Make a request to build an order with this ticket
+  const user = global.signin();
+  const { body: order } = await request(app)
+    .post('/api/orders')
+    .set('Cookie', user)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  // Make request to fetch the order with a different user
+  const { body: fetchOrder } = await request(app)
+    .get(`/api/orders/${order.id}`)
+    .set('Cookie', global.signin())
+    .send()
+    .expect(401);
+});
--- orders/src/routes/show.ts
+++ orders/src/routes/show.ts
@@ -1,9 +1,32 @@
 import express, { Request, Response } from 'express';
+import {
+  NotAuthorizedError,
+  NotFoundError,
+  requireAuth,
+} from '@goticket/common';
+import { Order } from '../models/orders';
 
 const router = express.Router();
 
-router.get('/api/orders/:orderId', async (req: Request, res: Response) => {
-  res.send({});
-});
+router.get(
+  '/api/orders/:orderId',
+  requireAuth,
+  async (req: Request, res: Response) => {
+    // Find the order with the order id and the associated ticket
+    const order = await Order.findById(req.params.orderId).populate('ticket');
+
+    // Check wheter the order exists
+    if (!order) {
+      throw new NotFoundError();
+    }
+
+    // Check if the order belongs to the current user
+    if (order.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+
+    res.send(order);
+  }
+);
 
 export { router as showOrderRouter };
