e2081abe4e8417499d7c051d9dc76792e8f45b44
amyhuang95 <62624198+amyhuang95@users.noreply.github.com>
2024-10-02 10:14:45 -0700

    Add id field when building tickets

--- orders/src/routes/__test__/show.test.ts
+++ orders/src/routes/__test__/show.test.ts
@@ -1,10 +1,12 @@
 import request from 'supertest';
 import { app } from '../../app';
 import { Ticket } from '../../models/ticket';
+import mongoose from 'mongoose';
 
 it('fetches the order', async () => {
   // Create a ticket
   const ticket = Ticket.build({
+    id: new mongoose.Types.ObjectId().toHexString(),
     title: 'test ticket title',
     price: 50,
   });
@@ -31,6 +33,7 @@ it('fetches the order', async () => {
 it("returns an error if one user tries to fetch the other user's order", async () => {
   // Create a ticket
   const ticket = Ticket.build({
+    id: new mongoose.Types.ObjectId().toHexString(),
     title: 'test ticket title',
     price: 50,
   });
