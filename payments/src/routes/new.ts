90bdaa74a8f6666d5babde0c33f40f4653f1cdac
amyhuang95 <62624198+amyhuang95@users.noreply.github.com>
2024-10-16 09:54:22 -0700

    Add payment model

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -10,6 +10,7 @@ import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 import { Order } from '../models/order';
 import { stripe } from '../stripe';
+import { Payment } from '../models/payment';
 
 const router = express.Router();
 
@@ -34,11 +35,18 @@ router.post(
     }
 
     // Create strip charges
-    await stripe.charges.create({
+    const charge = await stripe.charges.create({
       currency: 'usd',
       amount: order.price * 100, // convert to cents
       source: token, // where does the money come from
     });
+
+    // Build a new payment - for internal record
+    const payment = Payment.build({
+      orderId,
+      stripeId: charge.id,
+    });
+    await payment.save();
 
     res.status(201).send({ success: true });
   }
