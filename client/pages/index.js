de5c33d103b80d341dec6081bce4d0e8d2639935
amyhuang95 <62624198+amyhuang95@users.noreply.github.com>
2024-10-21 13:39:58 -0700

    load tickets data to root page

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,6 @@
 // Generate components with provided data
-const LandingPage = ({ currentUser }) => {
+const LandingPage = ({ currentUser, tickets }) => {
+  console.log(tickets);
   return currentUser ? (
     <h1>You are signed in</h1>
   ) : (
@@ -9,7 +10,9 @@ const LandingPage = ({ currentUser }) => {
 
 // Fetch data during server-side rendering process
 LandingPage.getInitialProps = async (context, client, currentUser) => {
-  return {};
+  const { data } = await client.get('/api/tickets');
+
+  return { tickets: data };
 };
 
 export default LandingPage;
