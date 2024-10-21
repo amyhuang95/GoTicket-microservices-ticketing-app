df3137fcf8cb7cdc0992e56b297ae7a070ff221f
amyhuang95 <62624198+amyhuang95@users.noreply.github.com>
2024-10-21 13:15:48 -0700

    Add input values

--- client/pages/tickets/new.js
+++ client/pages/tickets/new.js
@@ -1,19 +1,49 @@
 /**
  * Page to create a new ticket
  */
+import { useState } from 'react';
 
 const NewTicket = () => {
+  const [title, setTitle] = useState('');
+  const [price, setPrice] = useState('');
+
+  // auto-format numbers to 2 decimals
+  const onBlur = () => {
+    const value = parseFloat(price);
+
+    // Check if it's not a number
+    if (isNaN(value)) {
+      return;
+    }
+
+    // Set price to round to 2 decimals
+    setPrice(value.toFixed(2));
+  };
+
   return (
     <div>
       <h1>Create a new ticket</h1>
       <form>
         <div className="form-group">
           <label>Title</label>
-          <input className="form-control" />
+          <input
+            value={title}
+            onChange={(e) => {
+              setTitle(e.target.value);
+            }}
+            className="form-control"
+          />
         </div>
         <div className="form-group">
           <label>Price</label>
-          <input className="form-control" />
+          <input
+            value={price}
+            onBlur={onBlur} // a Blur event is when user click outside of the input field
+            onChange={(e) => {
+              setPrice(e.target.value);
+            }}
+            className="form-control"
+          />
         </div>
         <button className="btn btn-primary">Submit</button>
       </form>
