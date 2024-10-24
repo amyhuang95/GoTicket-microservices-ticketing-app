e5d96a0043f6f9a3b462e1a0af917b01dd83b658
amyhuang95 <62624198+amyhuang95@users.noreply.github.com>
2024-10-24 09:38:48 -0700

    add nav options for looking up orders and sell tickets

--- client/components/header.js
+++ client/components/header.js
@@ -5,26 +5,28 @@ const Header = ({ currentUser }) => {
   const links = [
     !currentUser && { label: 'Sign Up', href: '/auth/signup' },
     !currentUser && { label: 'Sign in', href: '/auth/signin' },
+    currentUser && { label: 'Sell tickets', href: '/tickets/new' },
+    currentUser && { label: 'My orders', href: '/orders' },
     currentUser && { label: 'Sign out', href: '/auth/signout' },
   ]
     .filter((linkConfig) => linkConfig)
     .map(({ label, href }) => {
       return (
-        <li key={href} className="nav-item">
-          <Link href={href} className="nav-link">
+        <li key={href} className='nav-item'>
+          <Link href={href} className='nav-link'>
             {label}
           </Link>
         </li>
       );
     });
   return (
-    <nav className="navbar navbar-light bg-light">
-      <Link className="navbar-brand" href="/">
+    <nav className='navbar navbar-light bg-light'>
+      <Link className='navbar-brand' href='/'>
         GoTicket
       </Link>
 
-      <div className="d-flex justify-content-end">
-        <ul className="nav d-flex align-items-center">{links}</ul>
+      <div className='d-flex justify-content-end'>
+        <ul className='nav d-flex align-items-center'>{links}</ul>
       </div>
     </nav>
   );
