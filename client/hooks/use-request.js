8db13da5eb856a24a84a1de6a35e66691a488aaf
amyhuang95 <62624198+amyhuang95@users.noreply.github.com>
2024-10-24 09:28:29 -0700

    add additional data to be merged into the request when doRequest is called

--- client/hooks/use-request.js
+++ client/hooks/use-request.js
@@ -4,19 +4,19 @@ import { useState } from 'react';
 const useRequest = ({ url, method, body, onSuccess }) => {
   const [errors, setErrors] = useState(null);
 
-  const doRequest = async () => {
+  const doRequest = async (props = {}) => {
     try {
       setErrors(null);
-      const { data } = await axios[method](url, body);
+      const { data } = await axios[method](url, { ...body, ...props });
       if (onSuccess) {
         onSuccess(data);
       }
       return data;
     } catch (err) {
       setErrors(
-        <div className="alert alert-danger">
+        <div className='alert alert-danger'>
           <h4>Oooops</h4>
-          <ul className="my-0">
+          <ul className='my-0'>
             {err.response.data.errors.map((error) => (
               <li key={error.message}>{error.message}</li>
             ))}
