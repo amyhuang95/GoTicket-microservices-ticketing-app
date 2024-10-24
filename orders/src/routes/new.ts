8afcbfd476f2bd0ae1a088ef4c9722c69009776c
amyhuang95 <62624198+amyhuang95@users.noreply.github.com>
2024-10-24 10:09:31 -0700

    set expiration window to 1 min

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -14,7 +14,7 @@ import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
-const EXPIRATION_WINDOW_SECONDS = 15 * 60; // 15 minutes * 60 seconds
+const EXPIRATION_WINDOW_SECONDS = 1 * 60; // 1 minutes * 60 seconds
 
 router.post(
   '/api/orders',
@@ -44,7 +44,7 @@ router.post(
 
     // Calculate an expiration date for this order
     const expiration = new Date();
-    expiration.setSeconds(expiration.getSeconds() + EXPIRATION_WINDOW_SECONDS); // 15 minutes from now
+    expiration.setSeconds(expiration.getSeconds() + EXPIRATION_WINDOW_SECONDS);
 
     // Build the order and save it to the database
     const order = Order.build({
